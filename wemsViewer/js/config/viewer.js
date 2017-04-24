d945fa4c57374e9bdb26defe2480204eceb5b22f
tebert <tebert@lirr.org>
2017-04-24 12:07:23 -0400

    https

--- wemsViewer/js/config/viewer.js
+++ wemsViewer/js/config/viewer.js
@@ -76,7 +76,7 @@ define([
         operationalLayers: [
 	{
             type: 'dynamic',
-            url: 'http://arcgisprod10.lirr.org/arcgis/rest/services/WEMS/MapServer', 
+            url: 'https://arcgisprod10.lirr.org/arcgis/rest/services/WEMS/MapServer', 
             title: 'WEMS',
             options: {
                 id: 'WEMS',
